5ce722cddcbfc2fadd9aa6298152a3803f33ce76
DESKTOP-TB4CQ85\Nirav Bavadiya <niravbavadiya97@gmail.com>
2025-10-06 22:20:10 +0200

    app gradles

---
+++ depressiondetectionstandalone/build.gradle.kts
@@ -0,0 +1,23 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        // Needed if you use Kotlin serialization or kapt later
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
